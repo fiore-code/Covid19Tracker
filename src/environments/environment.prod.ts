f03c6efd04b2cc1f9500bb7b1b0cdae6d47b604d
suparna biswas <subhambiswas38@gmail.com>
2020-06-16 18:59:15 +0530

    Production access token added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoic3ViaGFtLWZpb3JlIiwiYSI6ImNrOHl2Y3NkNDBsbm4zbHRhaGR6bndtNHgifQ.ljcjEqww1G0IHITR4MesbQ'
+  },
+  mapbox2:{
+    accessToken:'pk.eyJ1IjoiYml0YW5iMTIzIiwiYSI6ImNrOHlwdWlieTFrbW4zZ3FzN2Qwc3MzdmYifQ.qR6zhcdrOSS_k1h0yWzWJQ'
+  }
 };
